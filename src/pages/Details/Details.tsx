d44413873ebc6254f6cc40460f9ad35468d6d649
Соколов Д.А. <babushkian@mail.ru>
2025-02-14 17:35:23 +0600

    Детали программы сделал через таблицу, теперь выводятся все свойства в цикле

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,36 +1,43 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 
-export type ProgramPartsType = IProgramPartsRecord[]
+export type ProgramPartsType = IProgramPartsRecord[];
 export interface IProgramPartsRecord {
-    WONumber: string
-    ProgramName: string
-    PartName: string
-    RepeatID: number
-    QtyInProcess: number
-    PartLength: number
-    PartWidth: number
-    TrueArea: number
-    RectArea: number
-    TrueWeight: number
-    RectWeight: number
-    CuttingTime: number
-    CuttingLength: number
-    PierceQty: number
-    NestedArea: number
-    TotalCuttingTime: number
-    MasterPartQty: number
-    WOState: number
-    DueDate: string
-    RevisionNumber: string
-    PK_PIP: string
-  }
-  
-
+    WONumber: string;
+    ProgramName: string;
+    PartName: string;
+    RepeatID: number;
+    QtyInProcess: number;
+    PartLength: number;
+    PartWidth: number;
+    TrueArea: number;
+    RectArea: number;
+    TrueWeight: number;
+    RectWeight: number;
+    CuttingTime: number;
+    CuttingLength: number;
+    PierceQty: number;
+    NestedArea: number;
+    TotalCuttingTime: number;
+    MasterPartQty: number;
+    WOState: number;
+    DueDate: string;
+    RevisionNumber: string;
+    PK_PIP: string;
+}
 
 const Details = () => {
-    const data = useLoaderData() as ProgramPartsType
-    return data.map((record) => <div key={record.PK_PIP}>Детали задания {`${record.ProgramName}    ${record.PartName}  ${record.RepeatID}  ${record.QtyInProcess}`}</div>);
+    const data = useLoaderData() as ProgramPartsType;
+    const tableBody = data.map((record, index) => {
+        return (
+            <tr key={index}>
+                {Object.keys(record).map((cellData, cellindex) => (
+                    <td key={cellindex}> {record[cellData]}</td>
+                ))}
+            </tr>
+        );
+    });
+    return <table><tbody>{tableBody}</tbody></table>
 };
 
 export default Details;
