aea1336ba52dbd2c4613039c871a94ca3c978bd4
Соколов Д.А. <babushkian@mail.ru>
2025-02-28 14:41:38 +0600

    оказалось, что mui datagrid не дает применить несколько фильтров

--- src/pages/PartsByStatuses/PartsByStatuses.tsx
+++ src/pages/PartsByStatuses/PartsByStatuses.tsx
@@ -1,13 +1,21 @@
 import { useState, useEffect, useRef, useCallback } from "React";
 import { Box, Typography } from "@mui/material";
-import { DataGrid, GridRowsProp, GridColDef, GridToolbar, useGridApiRef } from "@mui/x-data-grid";
+import {
+    DataGrid,
+    GridRowsProp,
+    GridColDef,
+    GridToolbar,
+    useGridApiRef,
+    getGridStringOperators,
+    getGridNumericOperators,
+} from "@mui/x-data-grid";
 import { getPartsByStatuses } from "../../utils/requests";
 
 const TestLayout = () => {
     const [data, setData] = useState<any[]>([]);
     const [showData, setShowData] = useState(false);
     const columns = useRef(undefined);
-    const initStateRef = useRef(undefined)
+    const initStateRef = useRef(undefined);
     const apiRef = useGridApiRef();
     const loadData = async () => {
         try {
@@ -20,27 +28,55 @@ const TestLayout = () => {
         }
     };
 
+    const fieldsDict = {
+        ProgramName: "string",
+        UsedArea: "number",
+        Thickness: "number",
+        SheetLength: "number",
+        SheetWidth: "number",
+        CuttingLength: "number",
+        PierceQtyPart: "number",
+        Material: "string",
+    };
+
     const createColumns = () => {
-        return Object.keys(data[0]).map((colName, index) => {
-            return { field: colName, headerName: colName, flex: 1};
+        const fields = Object.keys(fieldsDict);
+        const columns = Object.keys(data[0]).map((colName) => {
+            if (fields.includes(colName)) {
+                return { field: colName, headerName: colName, flex: 1, type: fieldsDict[colName] };
+            } else {
+                return { field: colName, headerName: colName, flex: 1, type: "string" };
+            }
         });
+        return columns;
     };
 
     const setInitState = () => {
-        const initial = {}
-        const columnVis = {}
-        Object.keys(data[0]).forEach((colName, index) => {
-            let visibility = false
-            if (index < 7) {
-                visibility = true
+        const fields = Object.keys(fieldsDict);
+        const initial = {};
+        const columnVis = {};
+        Object.keys(data[0]).forEach((colName) => {
+            let visibility = false;
+            if (!fields.includes(colName)) {
+                columnVis[colName] = false;
             }
-            columnVis[colName] = visibility
-            
         });
-        initial["columns"] = {columnVisibilityModel:columnVis}
-        return initial
+        initial["columns"] = { columnVisibilityModel: columnVis };
+        console.log("операторы");
+        console.log(getGridStringOperators());
+        console.log(getGridNumericOperators());
+        initial["filter"] = {
+            filterModel: {
+                items: [
+                    // { field: "Material", operator: "contains", value: "gs" },
+                    // { field: "Thickness", operator: ">", value: 4 },
+                    { field: "PierceQtyPart", operator: "=", value: 2 },
+                ],
+            },
+        };
 
-    }
+        return initial;
+    };
 
     useEffect(() => {
         loadData();
@@ -49,9 +85,9 @@ const TestLayout = () => {
     useEffect(() => {
         if (data.length > 0) {
             columns.current = createColumns();
-            initStateRef.current = setInitState()
-            console.log("initialState")
-            console.log(initStateRef.current)
+            initStateRef.current = setInitState();
+            console.log("initialState");
+            console.log(initStateRef.current);
         }
     }, [data]);
 
@@ -72,7 +108,6 @@ const TestLayout = () => {
                         rows={data}
                         columns={columns.current}
                         initialState={initStateRef.current}
-
                         slots={{ toolbar: GridToolbar }}
                         apiRef={apiRef}
                         onStateChange={() => {
