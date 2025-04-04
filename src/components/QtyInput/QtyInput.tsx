4fe98da6fc6b1018b523465aa5ff8517cb864277
Соколов Д.А. <babushkian@mail.ru>
2025-04-04 09:58:01 +0600

    теперь состояние таблицы обновляется не на ввод каждой цифры, а только когда пользователь ушел из элемента. То есть закончил редактрование.

--- src/components/QtyInput/QtyInput.tsx
+++ src/components/QtyInput/QtyInput.tsx
@@ -1,4 +1,5 @@
 import { TextField } from "@mui/material";
+import { useState } from "react";
 type SetQtyType = {
     rowId: number;
     initialQty: number;
@@ -6,17 +7,25 @@ type SetQtyType = {
 };
 
 const QtyInput = ({ rowId, initialQty, applyQty }: SetQtyType) => {
+    const [qty, setQty] = useState(initialQty)
+    
     const hadleSetQty = (event: React.ChangeEvent<HTMLInputElement>) => {
         // если удалить из строки ввода всё, там появляется NaN, что приводит к ошибке
         // В этом случае нужно принудительно заменять NaN на 0
         const converted = Number.parseInt(event.target.value);
         const newValue = !isNaN(converted) ? converted : 0;
-        applyQty(rowId, newValue);
+        //applyQty(rowId, newValue);
+        setQty(newValue)
     };
+
+
+    const updateTable = () =>{
+        if (qty !== initialQty) applyQty(rowId, qty)
+    }
 
     return (
         <>
-            <TextField size="small" sx={{ margin: 1 }} value={initialQty} onChange={hadleSetQty} />
+            <TextField size="small" sx={{ margin: 1 }} value={qty} onBlur={updateTable}  onChange={hadleSetQty} />
         </>
     );
 };
